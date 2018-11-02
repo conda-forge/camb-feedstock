233194f8d13ecf52cdcb69f5695a64284a7f862c
Matthew R Becker <becker.mr@gmail.com>
2018-11-02 17:01:48 -0500

    try one final thing...

--- recipe/install-camb.bat
+++ recipe/install-camb.bat
@@ -1,5 +1,11 @@
 copy "%RECIPE_DIR%\install-camb.sh" .
 set CHERE_INVOKING=1
 bash -lc "./install-camb.sh"
+
+# doing a test here since conda-build doesn't test properly for windows
+# even when using a script!
+set CHERE_INVOKING=1
+camb params.ini
+
 if errorlevel 1 exit 1
 exit 0
