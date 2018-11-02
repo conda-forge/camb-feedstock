83b1201cd7dc2fa26f6889fe1b201ce1a4cf803b
Matthew R Becker <becker.mr@gmail.com>
2018-11-02 16:01:05 -0500

    windows tests

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -1,8 +1,4 @@
-copy "%RECIPE_DIR%\params.ini" .
-copy "%SRC_DIR%\camb" .
-copy "%RECIPE_DIR%\test_camb.sh" .
-copy "%SRC_DIR%\HighLExtrapTemplate_lenspotentialCls.dat" .
 set CHERE_INVOKING=1
-bash -lc "./test_camb.sh"
+camb params.ini
 if errorlevel 1 exit 1
 exit 0
