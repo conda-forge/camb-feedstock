397532824d358fb7ec1d177a5cc38d55c3b66a31
Matthew R Becker <becker.mr@gmail.com>
2018-11-06 08:04:15 -0600

    fake it till you make it

--- recipe/install-camb.bat
+++ recipe/install-camb.bat
@@ -5,7 +5,7 @@ bash -lc "./install-camb.sh"
 REM doing a test here since conda-build doesn't test properly for windows
 REM even when using a script!
 set CHERE_INVOKING=1
-bash -lc "./camb params.ini"
+bash -lc "source activate ${PREFIX} && ./camb params.ini"
 
 if errorlevel 1 exit 1
 exit 0
