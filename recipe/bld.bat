0c4e537cf0fcd4e0c1ba2e19ad464bef7ee48cf3
Matthew R. Becker <becker.mr@gmail.com>
2020-02-11 10:18:36 -0600

    windows calls linux

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,9 +3,3 @@ set CHERE_INVOKING=1
 bash -lc "./build.sh"
 if errorlevel 1 exit 1
 exit 0
-
-copy "%RECIPE_DIR%\install-pycamb.sh" .
-set CHERE_INVOKING=1
-bash -lc "./install-pycamb.sh"
-if errorlevel 1 exit 1
-exit 0
