e4eea177a3bea4de4b9dd28d9b94baf7ebc75073
Matthew R. Becker <beckermr@users.noreply.github.com>
2021-07-07 08:51:26 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,6 +7,12 @@ fi
 rm -rf forutils
 
 if [[ ${target_platform} == "osx-arm64" ]]; then
+    # help it find the right compiler
+    export F90C=${F90}
+    rm -f ${BUILD_PREFIX}/bin/gfortran
+    ln -s ${F90} ${BUILD_PREFIX}/bin/gfortran
+    
+    # we download by hand so we can patch the makefile of the submodule
     ${PYTHON} -c "import setup, os; os.chdir('fortran'); setup.get_forutils()"
     cp ${RECIPE_DIR}/Makefile_compiler forutils/.
 fi
