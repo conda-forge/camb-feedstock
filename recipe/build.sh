fb2b700abe2b85fafe735b32e7a6c73917eed59b
Matthew R. Becker <beckermr@users.noreply.github.com>
2021-07-07 05:59:52 -0500

    Update recipe/build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,12 +6,6 @@ fi
 
 rm -rf forutils
 
-if [[ "$CONDA_BUILD_CROSS_COMPILATION" == "1" ]]; then
-  # workaround until cross-python is fixed
-  rm $BUILD_PREFIX/bin/python
-  ln -sf $PREFIX/bin/python $BUILD_PREFIX/bin/python
-fi
-
 ${PYTHON} setup.py build_cluster
 
 ${PYTHON} -m pip install . -vv --no-deps --install-option="--skip-build"
